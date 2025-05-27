6224b9a0af3e08ac8d1c5eeb63bdc1228dfbdad0
onion-chen <chenshicong666@qq.com>
2025-05-27 12:30:15 +0800

    删除homepage

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,11 +1,9 @@
 import { defineClientConfig } from '@vuepress/client';
-import HomePage from './components/HomePage.vue';
 import Utterances from './components/Utterances.vue';
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     // 注册全局组件
-    app.component('HomePage', HomePage);
     app.component('Utterances', Utterances);
   },
   setup() {},
